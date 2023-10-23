e1c1ef3cfc9cf98109b7a3abe8a416c866aac4d2
FlorianSchoenfelder <f.schoenfelder22@gmail.com>
2023-10-23 18:55:30 +0200

    Quizapp done only with HTML question

--- startHTMLQuiz.js
+++ startHTMLQuiz.js
@@ -1,6 +1,6 @@
 let HTMLquestions = [
     {
-        "question": "Wer hat HTML erfunden?",
+        "question": "Who invented HTML?",
         "answer1": "Bruno Mars",
         "answer2": "Ludwig Erhard",
         "answer3": "Tim Berners-Lee",
@@ -8,7 +8,7 @@ let HTMLquestions = [
         "rightanswer": 3
     },
     {
-        "question": "Mit welchem Tag erschaffe ich einen Container?",
+        "question": "With which Tag can i create a container?",
         "answer1": "ul",
         "answer2": "div",
         "answer3": "img",
@@ -16,7 +16,7 @@ let HTMLquestions = [
         "rightanswer": 2
     },
     {
-        "question": "Welches Attribut kann ich nutzen um etwas zu Designen?",
+        "question": "To design something, which tag attribute can i use?",
         "answer1": "class",
         "answer2": "onclick",
         "answer3": "href",
@@ -24,7 +24,7 @@ let HTMLquestions = [
         "rightanswer": 1
     },
     {
-        "question": "Wie viele größen an Überschriften gibt es?",
+        "question": "How much headline tags do we have?",
         "answer1": "4",
         "answer2": "5",
         "answer3": "7",
@@ -32,7 +32,7 @@ let HTMLquestions = [
         "rightanswer": 4
     },
     {
-        "question": "Was bedeutet HTML genau?",
+        "question": "What exactly means HTML?",
         "answer1": "HypaText Markup Language",
         "answer2": "HyperText Markup Language",
         "answer3": "HyperText Makeup Language",
@@ -43,22 +43,20 @@ let HTMLquestions = [
 
 let currentQuestion = 0;
 let counterRightAnswer = 0;
+let AudioSuccess = new Audio('audio/success.mp3');
+let AudioFail = new Audio('/audio/fail.mp3');
 
 function startHTMLQuiz() {
-
-
-
     if (currentQuestion >= HTMLquestions.length) {
         //Init endscreen
         loadEndscreen();
     } else {
         let question = HTMLquestions[currentQuestion];
-
         document.getElementById('main-container').innerHTML = startHTMLQuizText(question);
         document.getElementById('currentquestion').innerHTML = currentQuestion + 1;
         document.getElementById('allquestion').innerHTML = HTMLquestions.length;
 
-        let percent = (currentQuestion +1) / HTMLquestions.length;
+        let percent = (currentQuestion + 1) / HTMLquestions.length;
         percent = Math.round(percent * 100);
         document.getElementById('progress-bar').innerHTML = `${percent} %`;
         document.getElementById('progress-bar').style = `width: ${percent}%;`;
@@ -80,21 +78,18 @@ function startHTMLQuizText(question) {
                     ${question['answer1']}
                 </div>
             </div>
-
             <div onclick="answer('answer_2')" class="card mb-3 w-75">
                 <div class="card-body" id="answer_2">
                     <div class="answer">B</div>
                     ${question['answer2']}
                 </div>
             </div>
-
             <div onclick="answer('answer_3')" class="card mb-3 w-75">
                 <div class="card-body" id="answer_3">
                     <div class="answer">C</div>
                     ${question['answer3']}
                 </div>
             </div>
-
             <div onclick="answer('answer_4')" class="card mb-3 w-75">
                 <div class="card-body" id="answer_4">
                     <div class="answer">D</div>
@@ -102,7 +97,6 @@ function startHTMLQuizText(question) {
                 </div>
             </div>
             <div class="previousAndNextBtn">
-                <button onclick="previousQuestion()" class="previousButton"></button>
                 <span>
                     <b id="currentquestion"></b> von
                     <b id="allquestion"></b> Fragen
@@ -118,26 +112,20 @@ function answer(selcetedAnswer) {
     let selcetedAnswerAsNumber = selcetedAnswer.slice(-1);
     let rightanswerID = `answer_${question['rightanswer']}`;
 
-
     if (selcetedAnswerAsNumber == question['rightanswer']) {
-        console.log('Du bist richtig gut!!!');
         document.getElementById(selcetedAnswer).parentNode.classList.add('bg-success');
+        AudioSuccess.play();
         counterRightAnswer++;
     } else {
-        console.log('Vielleicht beim nächsten mal.');
         document.getElementById(selcetedAnswer).parentNode.classList.add('bg-danger');
         document.getElementById(rightanswerID).parentNode.classList.add('bg-success');
+        AudioFail.play();
     }
     document.getElementById('nextButton').disabled = false;
 }
 
 function nextQuestion() {
     currentQuestion++;
-    startHTMLQuiz();
-}
-
-function previousQuestion() {
-    currentQuestion--;
     startHTMLQuiz();
 }
 
@@ -152,13 +140,18 @@ function loadEndscreen() {
                 <span>
                     <b>Complete</b> <br>
                     <b>HTML Quiz</b>
-                </span>
-                
+                </span>                
             </div>
             <div class="result">Your Score ${counterRightAnswer}/${HTMLquestions.length}</div>
             <div class="replay">
-                <button onclick="startHTMLQuizText()" class="btn btn-primary">Replay</button>
+                <button onclick="restartQuiz()" class="btn btn-primary">Replay</button>
             </div>
         </div>
-    `
+    `;
+}
+
+function restartQuiz() {
+    currentQuestion = 0;
+    counterRightAnswer = 0;
+    chooseQuiz('HTML', 1);
 }
